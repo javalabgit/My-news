ad7c80cc2209e0a88520edf1821a549a1b914ef5
javalabgit <mylappysilver@gmail.com>
2025-12-26 21:26:33 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { UserProfileProvider } from "./UserProfileContext"; // Import this
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <UserProfileProvider>
+      <App />
+    </UserProfileProvider>
   </React.StrictMode>
 );
